8aeba17ab7de911b29b003dde24c66a702a6a7bf
李艳楠 <xiangjunmiao@icar99.com>
2016-04-01 15:54:19 +0800

    检测终端命令

--- MZWeibo/MZWeibo/ViewController.swift
+++ MZWeibo/MZWeibo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("检测终端命令行")
     }
 
     override func didReceiveMemoryWarning() {
