ecb9a0ba5dd855b52a5e852fa5db5003797b5fab
李艳楠 <xiangjunmiao@icar99.com>
2016-04-05 11:29:05 +0800

    从服务器端动态加载控制器：重要

--- MZWeibo/MZWeibo/Classes/Main/MainViewController.swift
+++ MZWeibo/MZWeibo/Classes/Main/MainViewController.swift
@@ -14,14 +14,41 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
         //当前控制器对应tabBar的颜色
         tabBar.tintColor = UIColor.orangeColor()
-        //1、创建首页
-        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
-        //2、
-        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
-        //3、
-        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
-        //4、
-        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+        //1、获取JSON文件的路径
+        let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
+        //2、通过文件路径创建NSData，进行判断path 一定不为空
+        if let jsonPath = path{
+            let jsonData = NSData(contentsOfFile: jsonPath)
+            do
+            {
+                //有可能发生异常的代码放到这里
+                //3、序列化json数据 --> array
+                //try:发生异常会跳到catch中继续执行
+                //try!:发生异常程序会直接崩溃
+                let dictArr = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers)
+                print(dictArr)
+                //4、遍历数组，动态创建控制器和数据
+                //在Swift中，如果要遍历数组，必须明确数据的类型
+                for dict in dictArr as![[String: String]]
+                {
+                    //报错的原因：addChildViewController参数必须有值，但字典的返回值为可选类型
+                    addChildViewController(dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
+                }
+ 
+            }catch
+            {
+               //发生异常之后执行的代码
+                print(error)
+                //1、创建首页
+                addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+                //2、
+                addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+                //3、
+                addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+                //4、
+                addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+            }
+        }
     }
     /**
      初始化子控制器
@@ -52,7 +79,6 @@ class MainViewController: UITabBarController {
         navHome.addChildViewController(classVC)
         //3、将导航控制器添加到当前控制器上
         addChildViewController(navHome)
-
     }
     
     
