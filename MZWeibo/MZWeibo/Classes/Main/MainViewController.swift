470fb1d129bf140898823c2dabc4a5772aa78f43
李艳楠 <xiangjunmiao@icar99.com>
2016-04-05 10:09:05 +0800

    1、初始化项目 2、配置基本文件

---
+++ MZWeibo/MZWeibo/Classes/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  MZWeibo
+//
+//  Created by 李艳楠 on 16/4/5.
+//  Copyright © 2016年 Déesse. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+}
