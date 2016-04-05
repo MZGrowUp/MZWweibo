fddf4fb653be8a2ecda05e857a19d3958b405a98
李艳楠 <xiangjunmiao@icar99.com>
2016-04-05 23:09:29 +0800

    对barButtonItem的设置

---
+++ MZWeibo/MZWeibo/Classes/Common/UIBarButtonItem+category.swift
@@ -0,0 +1,21 @@
+//
+//  UIBarButtonItem+category.swift
+//  MZWeibo
+//
+//  Created by 李艳楠 on 16/4/5.
+//  Copyright © 2016年 Déesse. All rights reserved.
+//
+
+import UIKit
+
+extension UIBarButtonItem{
+    //如果在func的前面加上class，就相当于OC中的"+"
+    class func creatBarButton(imageName: String, target: AnyObject?, action: Selector)->UIBarButtonItem{
+        let btn = UIButton()
+        btn.setImage(UIImage(named: imageName), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: imageName + "_highlighted"), forState: UIControlState.Highlighted)
+        btn.addTarget(target, action: action, forControlEvents: UIControlEvents.TouchUpInside)
+        btn.sizeToFit()
+        return UIBarButtonItem(customView: btn)
+    }
+}
--- MZWeibo/MZWeibo/Classes/Home/HomeTableViewController.swift
+++ MZWeibo/MZWeibo/Classes/Home/HomeTableViewController.swift
@@ -17,6 +17,35 @@ class HomeTableViewController: BaseTableViewController {
         if !userLogin
         {
             visitorView?.setUpVisitorViewInfo(true, imageName: "", message: "")
+            return
         }
+        //初始化导航条
+        setUpNavgation()
     }
+    func leftBtnClick(){
+        print(__FUNCTION__)
+    }
+    func rightBtnClick(){
+        print(__FUNCTION__)
+    }
+    private func setUpNavgation(){
+    //"navigationbar_friendattention"
+        /*
+        navigationItem.leftBarButtonItem = creatBarButton("navigationbar_friendattention", target: self, action: "leftBtnClick")
+        navigationItem.rightBarButtonItem = creatBarButton("navigationbar_pop", target: self, action: "rightBtnClick")
+        */
+        navigationItem.leftBarButtonItem = UIBarButtonItem.creatBarButton("navigationbar_friendattention", target: self, action: "leftBtnClick")
+        navigationItem.rightBarButtonItem = UIBarButtonItem.creatBarButton("navigationbar_pop", target: self, action: "rightBtnClick")
+        
+    }
+    /*
+    private func creatBarButton(imageName: String, target: AnyObject?, action: Selector)->UIBarButtonItem{
+        let btn = UIButton()
+        btn.setImage(UIImage(named: imageName), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: imageName + "_highlighted"), forState: UIControlState.Highlighted)
+        btn.addTarget(target, action: action, forControlEvents: UIControlEvents.TouchUpInside)
+        btn.sizeToFit()
+        return UIBarButtonItem(customView: btn)
+    }
+*/
 }
